2cf5c74c5a855f88297258b648444cd7d47af553
ntlfy <152511114+ntlfy@users.noreply.github.com>
2024-05-12 21:56:21 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -41,12 +41,13 @@ setgid 65535
 setuid 65535
 stacksize 6291456 
 flush
+auth strong
 
 users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
 
 $(awk -F "/" '{print "auth strong\n" \
 "allow " $1 "\n" \
-"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
+"proxy -6 -n -a -p" $3 ":" $4 "\n" \
 "flush\n"}' ${WORKDATA})
 EOF
 }
@@ -109,11 +110,7 @@ EOF
 
 bash /etc/rc.local
 
-gen_proxy_file_for_user() {
-    cat >proxy.txt <<EOF
-$(awk -F "/" '{print $3 ":" $4}' ${WORKDATA})
-EOF
-}
+gen_proxy_file_for_user
 rm -rf /root/setup.sh
 rm -rf /root/3proxy-3proxy-0.8.6
 
